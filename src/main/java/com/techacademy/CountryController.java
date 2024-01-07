8d992841c10e4636420556e943d5876fdba190b1
Longdinium <rondobell0303@gmail.com>
2024-01-07 09:39:25 +0900

    サービス、コントローラーの修正

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -3,7 +3,10 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("country")
@@ -22,5 +25,55 @@ public class CountryController {
         // country/list.htmlに画面遷移
         return "country/list";
     }
+
+
+    // ----- 追加:ここから Lesson16 Chapter5.2 -----
+    // ----- 詳細画面 -----
+    @GetMapping(value = { "/detail", "/detail/{code}/" } )
+    public String getCountry(@PathVariable(name = "code", required = false) String code, Model model) {
+        // （URLで？）codeが指定されていたら検索結果を、無ければ空のクラスを設定 -> つまり三項演算子
+        // 必ず Country オブジェクトをModelに登録することで、detail.html テンプレートの方でエラーになることを防いでいます。
+
+        Country country = code != null ? service.getCountry(code) : new Country();
+        // Modelに登録
+        model.addAttribute("country", country);
+        // country/detail.htmlに画面遷移
+        return "country/detail";
+    }
+
+    // ----- 更新（追加）　-----
+    @PostMapping("/detail")
+    public String postCountry(@RequestParam("code") String code, @RequestParam("name") String name, @RequestParam("population") int population, Model model) {
+        /*
+        画面のformで入力した値をパラメータとして受け取り、サービスの updateCountry() メソッドで更新処理。
+        codeの値が同じである（＝すでに登録されている）レコードがない場合には追加処理が行われる。
+        */
+        // 更新（追加）の実行
+        service.updateCountry(code, name, population);
+
+        // 一覧画面にリダイレクト
+        return "redirect:/country/list";
+    }
+
+
+    // ----- 削除画面 -----
+    @GetMapping("/delete")
+    public String deleteCountryForm(Model model) {
+        // country/delete.htmlに画面遷移
+        return "country/delete";
+    }
+
+
+    // ----- 削除 -----
+    @PostMapping("/delete")
+    public String deleteCountry(@RequestParam("code") String code, Model model) {
+        // 削除
+        service.deleteCountry(code);
+
+        // 一覧画面にリダイレクト
+        return "redirect:/country/list";
+    }
+
+    // ----- 追加:ここまで -----
 
 }
--- src/main/java/com/techacademy/CountryService.java
+++ src/main/java/com/techacademy/CountryService.java
@@ -1,13 +1,33 @@
 package com.techacademy;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/*
+ このクラスでJpaRepositoryに用意されている以下のメソッドを呼び出して使用するメソッドを定義する
+ findAll(): 全件検索
+ findById(): 1件検索（主キーによる検索）
+ save(): 作成、更新
+ deleteById(): 1件削除（主キーによる削除）
+  */
 
 @Service
+/*
+ このアノテーションによりサービスクラスであることを示している
+ @ServiceをつけたクラスはSpring起動時にDIコンテナ―へ登録される→ここよく分からない
+ */
 public class CountryService {
     private final CountryRepository repository;
+    /*
+     Spring Frameworkの管理下に置くクラスを「コンポーネント」と呼ぶが、ここではそれを利用
+     private finalで変数を定義？
+     →リポジトリから引数付きコンストラクタを用意、DIコンテナーを経由してインスタンスを取得
+     ここではrepository変数でコンポーネントの使用を定義
+     */
 
     @Autowired
     public CountryService(CountryRepository repository) {
@@ -19,4 +39,43 @@ public class CountryService {
         // リポジトリのfindAllメソッドを呼び出す
         return repository.findAll();
     }
+
+    // ----- 追加：ここから（Lesson16 Chapter5.1） -----
+
+    // （特定の？）1件を（Codeをキーに）検索して返す
+    public Country getCountry(String code) {
+        // findById検索するメソッドを引っ張ってくる
+        Optional<Country> option = repository.findById(code);
+        // 取得できなかった場合はnullを返す
+        Country country = option.orElse(null); // ここよく分からん
+        return country;
+    }
+        /*
+         OptionalはJavaの構文？　そういうものだと思っておく
+         ->どうやらifブロックを使う必要のあったnullチェックを簡潔にすることができる
+         （例）
+         Optional<T> オブジェクト名 = Optional.ofNullable(nullの可能性があるオブジェクト名);
+
+         ここでは、orElse(null) で取得できなかった場合に null を返す
+
+         参考 https://www.sejuku.net/blog/60892
+         */
+
+
+    // 更新（追加）を行う
+    @Transactional
+    // @Transactionalアノテーションは該当クラスのすべてのメソッドをトランザクション管理対象に設定します
+    // なおこのアノテーションをつけたメソッドはオートコミットがOFFとなり、コミットはメソッドが終わる際に自動で行われる
+    public void updateCountry(String code, String name, int population) {
+        Country country = new Country(code, name, population);
+        repository.save(country);
+    }
+
+    // 削除を行う
+    @Transactional
+    public void deleteCountry(String code) {
+        repository.deleteById(code);
+    }
+ // ----- 追加：ここまで（Lesson16 Chapter5.1） -----
+
 }
